aeea7830f0e82471dfc5f8aab9c0dffdb6fe5ac4
Narek Hovhanisyan <71082048+hovnarek90@users.noreply.github.com>
2022-06-25 23:54:38 +0400

    changed calculatorModal (#2)

--- src/pages/CalculatorModal/CalculatorModal.js
+++ src/pages/CalculatorModal/CalculatorModal.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./calculatorModal.css";
+import usa from "../img/usa.png";
+import russia from "../img/russia.png";
+import china from "../img/china.png";
+import uk from "../img/uk.png";
+import dubai from "../img/dubai.png";
 
 export const CalculatorModal = () => {
   return (
@@ -13,23 +18,42 @@ export const CalculatorModal = () => {
         փաստացի քաշի, բացառությամբ գերծավալային քաշով բեռների:
       </p>
       <div className="flags">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={usa} alt="" />
+        <img src={russia} alt="" />
+        <img src={uk} alt="" />
+        <img src={china} alt="" />
+        <img src={dubai} alt="" />
       </div>
       <div className="shipping-method">
         <p>Առաքման եղանակ</p>
         <div className="shipping-method-air">
-            <input type="radio" name="shipping-type" id="shipping-type" />
-            <label htmlFor="shipping-type"><i className="fa fa-plane"></i></label>
+          <input type="radio" name="shipping-type" id="shipping-type" />
+          <label htmlFor="shipping-type">
+            <i className="fa fa-plane"></i>
+          </label>
         </div>
         <div className="shipping-method-ship">
-            <input type="radio" name="shipping-type" id="shipping-type" />
-            <label htmlFor="shipping-type"><i className="fa fa-ship"></i></label>
+          <input type="radio" name="shipping-type" id="shipping-type" />
+          <label htmlFor="shipping-type">
+            <i className="fa fa-ship"></i>
+          </label>
         </div>
-      </div>
+        </div>
+        <div className="extra-weight">
+          <label htmlFor="extra-weight">
+            <input type="checkbox" name="" id="extra-weight" />
+            գերծավալային քաշ
+          </label>
+          <div className="input-number">
+            <input type="number" name="" id="" />
+            <input type="number" name="" id="" />
+          </div>
+          <div className="cost">
+            <p>Առաքման արժեք</p>
+            <p>{}դր</p>
+          </div>
+        </div>
+     
     </div>
   );
 };
