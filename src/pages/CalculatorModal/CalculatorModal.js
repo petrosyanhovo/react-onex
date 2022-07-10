deaf555ab160461883db5367c2ff2eba16649d78
Lilit Hovhannisyan <lilithovhannisyan.v@gmail.com>
2022-07-10 20:14:54 +0400

    calculator logic

--- src/pages/CalculatorModal/CalculatorModal.js
+++ src/pages/CalculatorModal/CalculatorModal.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./calculatorModal.css";
 import usa from "./calculatorModal_img/usa.png";
 import russia from "./calculatorModal_img/russia.png";
 import china from "./calculatorModal_img/china.png";
 import uk from "./calculatorModal_img/uk.png";
-import dubai from "./calculatorModal_img/dubai.png";
-import { Header } from "../../components/header/Header";
+import uae from "./calculatorModal_img/dubai.png";
 
 export const CalculatorModal = ({ calculatorModalShow }) => {
+
+    // const [isChecked, setIsChecked] = useState(false);
+    // const [isMethodChecked, setIsMethodChecked] = useState(false);
+    const [flagName, setFlagName] = useState(""); 
+    const [shippingMethod, setShippingMethod] = useState("");
+    const [weight, setWeight] = useState("");
+    const [shippingCost, setShippingCost] = useState("");
+
+
+    useEffect(() => {
+        checkCountry();
+    }, [weight]);
+
+    const checkCountry = () =>{
+        switch (true) {
+            case (flagName === "usa" && shippingMethod === "plane"): setShippingCost(weight * 6000);
+                break;
+            case (flagName === "usa" && shippingMethod === "ship"): setShippingCost(weight * 15000);
+                break;
+            case (flagName === "rus"): setShippingCost(weight * 2000);
+                break;
+            case (flagName === "uk"): setShippingCost(weight * 6000);
+                break;
+            case (flagName === "china" && shippingMethod === "plane"): setShippingCost(weight * 8000);
+                break;
+            case (flagName === "china" && shippingMethod === "ship"): setShippingCost(weight * 1500);
+                break;
+            case (flagName === "uae"): setShippingCost(weight * 5000);
+                break;
+            default: setShippingCost(weight * 6000);
+        }
+    }
+
     return (
         <div className="calculator-background">
             <div className="calculator-modal">
@@ -22,32 +54,78 @@ export const CalculatorModal = ({ calculatorModalShow }) => {
                     ըստ բեռի փաստացի քաշի, բացառությամբ գերծավալային քաշով
                     բեռների:
                 </p>
-                <div className="flags">
+                <div className="calculator-flags">
                     <ul>
                         <li>
-                          <label className="label-country">
-                            <img src={usa} alt="" />
-                          </label>
+                            <input 
+                                type="radio" 
+                                id="usa" 
+                                name="flag" 
+                                value="usa" 
+                                defaultChecked
+                                onClick={(e) => {
+                                    // setIsChecked(e.target.checked);
+                                    setFlagName(e.target.value)
+                                    }} />
+                            <label htmlFor="usa" className="label-country">
+                                <img src={usa} alt="" />
+                            </label>
                         </li>
                         <li>
-                          <label className="label-country">
-                            <img src={russia} alt="" />
-                          </label>
+                            <input 
+                                type="radio" 
+                                id="rus" 
+                                name="flag" 
+                                value="rus"                                 
+                                onClick={(e) => {
+                                    // setIsChecked(e.target.checked);
+                                    setFlagName(e.target.value)
+                                    }} />
+                            <label htmlFor="rus" className="label-country">
+                                <img src={russia} alt="" />
+                            </label>
                         </li>
                         <li>
-                          <label className="label-country">
-                            <img src={uk} alt="" />
-                          </label>
+                            <input 
+                                type="radio" 
+                                id="uk" 
+                                name="flag" 
+                                value="uk"
+                                onClick={(e) => {
+                                    // setIsChecked(e.target.checked);
+                                    setFlagName(e.target.value)
+                                    }} />
+                            <label htmlFor="uk" className="label-country">
+                                <img src={uk} alt="" />
+                            </label>
                         </li>
                         <li> 
-                          <label className="label-country">
-                            <img src={china} alt="" />
-                          </label>
+                            <input 
+                                type="radio" 
+                                id="china" 
+                                name="flag" 
+                                value="china"
+                                onClick={(e) => {
+                                    // setIsChecked(e.target.checked);
+                                    setFlagName(e.target.value)
+                                    }} />
+                            <label htmlFor="china" className="label-country">
+                                <img src={china} alt="" />
+                            </label>
                         </li>
                         <li>
-                          <label className="label-country">
-                            <img src={dubai} alt="" />
-                          </label>
+                            <input 
+                                type="radio" 
+                                id="uae" 
+                                name="flag" 
+                                value="uae"
+                                onClick={(e) => {
+                                    // setIsChecked(e.target.checked);
+                                    setFlagName(e.target.value)
+                                    }} />
+                            <label htmlFor="uae" className="label-country">
+                                <img src={uae} alt="" />
+                            </label>
                         </li>
                     </ul>
                     
@@ -59,9 +137,15 @@ export const CalculatorModal = ({ calculatorModalShow }) => {
                         <input
                             type="radio"
                             name="shipping-type"
-                            id="shipping-plane"
+                            id="plane"
+                            value="plane" 
+                            defaultChecked
+                            onClick={(e) => {
+                                // setIsMethodChecked(e.target.checked);
+                                setShippingMethod(e.target.value)
+                                }}
                         />
-                        <label htmlFor="shipping-plane">
+                        <label htmlFor="plane">
                             <i className="fa fa-plane"></i>
                         </label>
                     </div>
@@ -69,9 +153,15 @@ export const CalculatorModal = ({ calculatorModalShow }) => {
                         <input
                             type="radio"
                             name="shipping-type"
-                            id="shipping-ship"
+                            id="ship"
+                            value="ship" 
+                            onClick={(e) => {
+                                // setIsMethodChecked(e.target.checked);
+                                setShippingMethod(e.target.value);
+                                console.log(e);
+                                }}
                         />
-                        <label htmlFor="shipping-ship">
+                        <label htmlFor="ship">
                             <i className="fa fa-ship"></i>
                         </label>
                     </div>
@@ -91,6 +181,7 @@ export const CalculatorModal = ({ calculatorModalShow }) => {
                             name=""
                             id="input-number"
                             placeholder="փաստացի քաշ"
+                            onChange={(e) => setWeight(e.target.value)}
                         />
                         <select name="" id="input-select">
                             <option value="cm" defaultValue>
@@ -101,7 +192,7 @@ export const CalculatorModal = ({ calculatorModalShow }) => {
                     </div>
                     <div className="cost">
                         <p>Առաքման արժեք</p>
-                        <p>{"200"} դր</p>
+                        <p className="delivery-cost">{shippingCost} դր</p>
                     </div>
                 </div>
             </div>
