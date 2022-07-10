1216ca6d2cdee69bd307cd0a222ce5f8bd3a6a9d
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2022-07-10 14:59:41 +0400

    Added NavBarShow Function for media,(not completed)

---
+++ src/components/header/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = ({navBarShow}) => {
+  return (
+    <>
+        <nav className="nav">
+            <a href="#">Ներմուծում</a>
+            <a href="#">Արտահանում</a>
+            <a href="#">Կորպորատիվ</a>
+            <a href="#">Մեծածախի հարցում</a>
+            <a href="#">SMART Լուծումներ</a>
+          </nav>
+    </>
+  )
+}
+
+export default NavBar
