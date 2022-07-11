23f3135df0ec59b3e447620f695a9550cd356df3
Narek Hovhannisyan <hovnarek@gmail.com>
2022-07-11 17:36:33 +0400

    edited nav bar

--- src/components/header/NavBar/NavBar.js
+++ src/components/header/NavBar/NavBar.js
@@ -2,31 +2,28 @@ import React from "react";
 import "./NavBar.css";
 import { useState } from "react";
 
-const NavBar = ({ navBarShow }) => {
-    const [isActive, setActive] = useState(true);
-   
-
-    return (
-        <>
-            <nav className={isActive ? "nav active" : "nav"}>
-                <a className="link" href="#">
-                    Ներմուծում
-                </a>
-                <a className="link" href="#">
-                    Արտահանում
-                </a>
-                <a className="link" href="#">
-                    Կորպորատիվ
-                </a>
-                <a className="link" href="#">
-                    Մեծածախի հարցում
-                </a>
-                <a className="link" href="#">
-                    SMART Լուծումներ
-                </a>
-            </nav>
-        </>
-    );
+const NavBar = () => {
+  return (
+    <>
+      <nav className="nav ">
+        <a className="link" href="#">
+          Ներմուծում
+        </a>
+        <a className="link" href="#">
+          Արտահանում
+        </a>
+        <a className="link" href="#">
+          Կորպորատիվ
+        </a>
+        <a className="link" href="#">
+          Մեծածախի հարցում
+        </a>
+        <a className="link" href="#">
+          SMART Լուծումներ
+        </a>
+      </nav>
+    </>
+  );
 };
 
 export default NavBar;
