ec61cee83552dc7e5ebd284f6469910f6bb2cbfc
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2022-07-13 02:09:44 +0400

    Added Dropdown to Navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,77 +8,81 @@ import { CalculatorModal } from "../../pages/CalculatorModal/CalculatorModal";
 import NavBar from "./NavBar/NavBar";
 import { HeaderNavBar } from "./HeaderNavBar/HeaderNavBar";
 export const Header = () => {
-  const [isCalculatorModalShow, setCalculatorModalShow] = useState(false);
-  const [isNavBarShow, setNavBarShow] = useState(true);
-  //   const [isActive, setActive] = useState(true);
+    const [isCalculatorModalShow, setCalculatorModalShow] = useState(false);
+    const [isNavBarShow, setNavBarShow] = useState(true);
+    //   const [isActive, setActive] = useState(true);
 
-  const calculatorModalShow = () => {
-    setCalculatorModalShow(!isCalculatorModalShow);
-  };
+    const calculatorModalShow = () => {
+        setCalculatorModalShow(!isCalculatorModalShow);
+    };
 
-  const navBarShow = () => {
-    setNavBarShow(!isNavBarShow);
-  };
+    const navBarShow = () => {
+        setNavBarShow(!isNavBarShow);
+    };
 
-  //   const handleClick = (e) => {
-  //     console.log(e);
-  //   };
+    //   const handleClick = (e) => {
+    //     console.log(e);
+    //   };
 
-  return (
-    <header>
-      <div className="first-part">
-        <p>
-          Ռուսաստանից առաքումների որակը զգալի բարձրացնելու նպատակով՝ փոխվել է ՌԴ
-          պահեստի <span>հասցեն</span> ։
-        </p>
-      </div>
-      <div className="second-part">
-        <div className="link">
-          <Link to="/contact">Հետադարձ կապ </Link>|
-          <a href="https://onex.am/pages/terms">Պայմաններ </a>|
-          <a href="">(060)750-350</a>|{/* modal  */}
-          <Link to="/shops">Խանութներ</Link>
-          <a href="#">
-            <img src={armenia} /> Հայերեն
-          </a>
-        </div>
-        <div className="login-btn">
-          <Link to="/login" className="btn-1">
-            <i className="fa fa-sign-in"></i>
-            ՄՈՒՏՔ
-          </Link>
-          <Link to="/register" className="btn-2">
-            <i className="fa fa-user-plus"></i>
-            ԳՐԱՆՑՎԵԼ
-          </Link>
-        </div>
-      </div>
-      <div className="third-part">
-        <div className="hamburger fa-solid fa-bars " onClick={navBarShow}></div>
-        <div className="logo">
-          <Link className="first" to="/">
-            <img src={onex} alt="onex" />
-          </Link>
-          <Link className="second" to="/">
-            <img src={onex2} alt="onex" />
-          </Link>
-        </div>
-        <div className="right">
-          {isNavBarShow && <NavBar />}
-          <div className="calc-and-search">
-            <a onClick={calculatorModalShow}>
-              <i className="fa-solid fa-calculator"></i>
-            </a>
-            <Link to="/search">
-              <i className="fa-solid fa-search"></i>
-            </Link>
-          </div>
-        </div>
-      </div>
+    return (
+        <header>
+            <div className="first-part">
+                <p>
+                    Ռուսաստանից առաքումների որակը զգալի բարձրացնելու նպատակով՝
+                    փոխվել է ՌԴ պահեստի <span>հասցեն</span> ։
+                </p>
+            </div>
+            <div className="second-part">
+                <div className="link">
+                    <Link to="/contact">Հետադարձ կապ </Link>|
+                    <a href="https://onex.am/pages/terms">Պայմաններ </a>|
+                    <a href="">(060)750-350</a>|{/* modal  */}
+                    <Link to="/shops">Խանութներ</Link>
+                    <a href="#">
+                        <img src={armenia} /> Հայերեն
+                    </a>
+                </div>
+                <div className="login-btn">
+                    <Link to="/login" className="btn-1">
+                        <i className="fa fa-sign-in"></i>
+                        ՄՈՒՏՔ
+                    </Link>
+                    <Link to="/register" className="btn-2">
+                        <i className="fa fa-user-plus"></i>
+                        ԳՐԱՆՑՎԵԼ
+                    </Link>
+                </div>
+            </div>
+            <div className="third-part">
+                <div
+                    className="hamburger fa-solid fa-bars "
+                    onClick={navBarShow}
+                ></div>
+                <div className="logo">
+                    <Link className="first" to="/">
+                        <img src={onex} alt="onex" />
+                    </Link>
+                    <Link className="second" to="/">
+                        <img src={onex2} alt="onex" />
+                    </Link>
+                </div>
+                <div className="right">
+                    <HeaderNavBar />
+                    {/* {isNavBarShow && <NavBar />} */}
+                    <div className="calc-and-search">
+                        <a onClick={calculatorModalShow}>
+                            <i className="fa-solid fa-calculator"></i>
+                        </a>
+                        <Link to="/search">
+                            <i className="fa-solid fa-search"></i>
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
-      {isCalculatorModalShow && (
-        <CalculatorModal calculatorModalShow={calculatorModalShow} />
-      )}
-    </header>
-  );
+            {isCalculatorModalShow && (
+                <CalculatorModal calculatorModalShow={calculatorModalShow} />
+            )}
+        </header>
+    );
 };
--- src/components/header/HeaderNavBar/HeaderNavBar.js
+++ src/components/header/HeaderNavBar/HeaderNavBar.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { a } from "react-router-dom";
 import "./HeaderNavBar.css";
 export const HeaderNavBar = () => {
-  return (
-    <div class="navbar">
-      <a href="#home">Home</a>
-      <a href="#news">News</a>
-      <div class="dropdown">
-        <button class="dropbtn">
-          Dropdown
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+    return (
+        <div className="navbar">
+            <div className="dropdown">
+                <button className="dropbtn">
+                    Ներմուծում
+                    {/* <i className="fa fa-caret-down"></i> */}
+                </button>
+                <div className="dropdown-content">
+                    <a href="#">Օնլայն գնումներ</a>
+                    <a href="#">Ծանրոցներ ՌԴ - ից</a>
+                    <a href="#">Ծանրոցներ Նյու Յորքից</a>
+                    <a href="#">Գնիր իմ փոխարեն</a>
+                    <a href="#">Խանութներ</a>
+                </div>
+            </div>
+
+            <div className="dropdown">
+                <button className="dropbtn">
+                    Արտահանում
+                    {/* <i className="fa fa-caret-down"></i> */}
+                </button>
+                <div className="dropdown-content">
+                    <a href="#">Դեպի Ռուսաստան</a>
+                    <a href="#">Դեպի ԱՄՆ</a>
+                    <a href="#">Փաստաթղթեր</a>
+                </div>
+            </div>
+
+            <a href="#" className="link">Կորպորատիվ</a>
+            <a href="#" className="link">Մեծածախի հարցում</a>
+
+            <div className="dropdown">
+                <button className="dropbtn">
+                    SMART Լուծումներ
+                    {/* <i className="fa fa-caret-down"></i> */}
+                </button>
+                <div className="dropdown-content">
+                    <a href="#">ONEX Smart Wall</a>
+                    <a href="#">ONEX Smart Lockers</a>
+                    <a href="#">ONEX Smart Services</a>
+                    <a href="#">Dropify</a>
+                    <a href="#">OneClick Extension</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
