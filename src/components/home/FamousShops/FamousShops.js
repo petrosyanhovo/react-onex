286cc27e9712f998f0b0864ff49cb1606dac8ce5
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2022-07-15 23:45:56 +0400

    Added FamousShops media querries

--- src/components/home/FamousShops/FamousShops.js
+++ src/components/home/FamousShops/FamousShops.js
@@ -13,7 +13,11 @@ function importAll(r) {
 }
 
 const images = importAll(
-    require.context("../FamousShops/famousShops_img", false, /\.(png|jpe?g|svg)$/)
+    require.context(
+        "../FamousShops/famousShops_img",
+        false,
+        /\.(png|jpe?g|svg)$/
+    )
 );
 
 const FamousShops = () => {
@@ -21,75 +25,82 @@ const FamousShops = () => {
         <div className="famous-shops">
             <Heading
                 heading="ԼԱՎԱԳՈՒՅՆ ԽԱՆՈՒԹՆԵՐ"
-                text="Միջազգային օնլայն խանութների 90%-ը առաքում չեն իրականացնում դեպի Հայաստան," 
-                text2 = "սակայն ONEX-ի միջոցով կարող եք ստանալ Ձեր առաքանիները Հայաստանում ու Արցախում՝ արագ և ապահով"
+                text="Միջազգային օնլայն խանութների 90%-ը առաքում չեն իրականացնում դեպի Հայաստան,"
+                text2="սակայն ONEX-ի միջոցով կարող եք ստանալ Ձեր առաքանիները Հայաստանում ու Արցախում՝ արագ և ապահով"
             />
 
             <div className="shops">
-                <div className="shop-logo">
-                    <a href="http://www.amazon.com" target="blank">
-                        <img src={images["amazon.png"]} alt="" />
-                    </a>
+                <div className="shop-logos">
+                    <div className="shop-logo">
+                        <a href="http://www.amazon.com" target="blank">
+                            <img src={images["amazon.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.aliexpress.com" target="blank">
+                            <img src={images["aliexpress.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.ebay.com" target="blank">
+                            <img src={images["ebay.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.carters.com" target="blank">
+                            <img src={images["carters.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.victoriassecret.com" target="blank">
+                            <img src={images["vs.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.gap.com" target="blank">
+                            <img src={images["gap.png"]} alt="" />
+                        </a>
+                    </div>
                 </div>
-                <div className="shop-logo">
-                    <a href="http://www.aliexpress.com" target="blank">
-                        <img src={images["aliexpress.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.ebay.com" target="blank">
-                        <img src={images["ebay.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.carters.com" target="blank">
-                        <img src={images["carters.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.victoriassecret.com" target="blank">
-                        <img src={images["vs.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.gap.com" target="blank">
-                        <img src={images["gap.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.michaelkors.com" target="blank">
-                        <img src={images["mk.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="http://www.forever21.com" target="blank">
-                        <img src={images["forever21.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="https://www2.hm.com/en_gb/index.html" target="blank">
-                        <img src={images["hm.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="https://www.ralphlauren.com/" target="blank">
-                        <img src={images["rl.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="https://www.ozon.ru/" target="blank">
-                        <img src={images["ozon.png"]} alt="" />
-                    </a>
-                </div>
-                <div className="shop-logo">
-                    <a href="https://www.taobao.com/" target="blank">
-                        <img src={images["taobao.png"]} alt="" />
-                    </a>
+                <div className="shop-logos">
+                    <div className="shop-logo">
+                        <a href="http://www.michaelkors.com" target="blank">
+                            <img src={images["mk.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="http://www.forever21.com" target="blank">
+                            <img src={images["forever21.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a
+                            href="https://www2.hm.com/en_gb/index.html"
+                            target="blank"
+                        >
+                            <img src={images["hm.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="https://www.ralphlauren.com/" target="blank">
+                            <img src={images["rl.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="https://www.ozon.ru/" target="blank">
+                            <img src={images["ozon.png"]} alt="" />
+                        </a>
+                    </div>
+                    <div className="shop-logo">
+                        <a href="https://www.taobao.com/" target="blank">
+                            <img src={images["taobao.png"]} alt="" />
+                        </a>
+                    </div>
                 </div>
             </div>
 
-            <Link to='/shops' >
-                <Button buttonName="Բոլոր խանութները" />    
+            <Link to="/shops">
+                <Button buttonName="Բոլոր խանութները" />
             </Link>
         </div>
     );
