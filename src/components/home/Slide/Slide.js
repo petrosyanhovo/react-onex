7c197d3cb2498b5a069dc9449d38dc04d96b4d5c
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2022-07-19 20:11:47 +0400

    Changed Slider's media querries

--- src/components/home/Slide/Slide.js
+++ src/components/home/Slide/Slide.js
@@ -9,34 +9,34 @@ import img4 from "../Slide/img/img4.jpg";
 const images = [img1, img2, img3, img4];
 
 export default class AutoPlay extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 8000,
-      autoplaySpeed: 2000,
-      cssEase: "linear",
-    };
-    return (
-      <>
-        <Slide {...settings}>
-          <div className="img-div">
-            <img src={img1} alt="" />{" "}
-          </div>
-          <div className="img-div">
-            <img src={img2} alt="" />{" "}
-          </div>{" "}
-          <div className="img-div">
-            <img src={img3} alt="" />{" "}
-          </div>{" "}
-          <div className="img-div">
-            <img src={img4} alt="" />{" "}
-          </div>{" "}
-        </Slide>
-      </>
-    );
-  }
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 1000,
+            autoplaySpeed: 3000,
+            cssEase: "linear",
+        };
+        return (
+            <div className="slider">
+                <Slide {...settings}>
+                    <div className="img-div">
+                        <img src={img1} alt="" />{" "}
+                    </div>
+                    <div className="img-div">
+                        <img src={img2} alt="" />{" "}
+                    </div>{" "}
+                    <div className="img-div">
+                        <img src={img3} alt="" />{" "}
+                    </div>{" "}
+                    <div className="img-div">
+                        <img src={img4} alt="" />{" "}
+                    </div>{" "}
+                </Slide>
+            </div>
+        );
+    }
 }
