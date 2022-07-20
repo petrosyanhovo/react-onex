b8fbbb28d973238c1ad8d36cce21f1d6123415c1
Lilit Hovhannisyan <lilithovhannisyan.v@gmail.com>
2022-07-20 12:25:40 +0400

    change orders table

--- src/pages/Orders/OrdersList/OrdersList.js
+++ src/pages/Orders/OrdersList/OrdersList.js
@@ -1,75 +1,64 @@
+
 import { Table } from 'antd';
 import React from 'react';
 const columns = [
   {
     title: 'Name',
     dataIndex: 'name',
-    filters: [
-      {
-        text: 'Joe',
-        value: 'Joe',
-      },
-      {
-        text: 'Category 1',
-        value: 'Category 1',
-      },
-      {
-        text: 'Category 2',
-        value: 'Category 2',
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value, record) => record.address.startsWith(value),
-    width: '30%',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    sorter: (a, b) => a.age - b.age,
+    title: 'Chinese Score',
+    dataIndex: 'chinese',
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    filters: [
-      {
-        text: <span>London</span>,
-        value: 'London',
-      },
-      {
-        text: <span>New York</span>,
-        value: 'New York',
-      },
-    ],
-    onFilter: (value, record) => record.address.startsWith(value),
-    filterSearch: true,
-    width: '40%',
+    title: 'Math Score',
+    dataIndex: 'math',
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: 'English Score',
+    dataIndex: 'english',
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
   },
 ];
 const data = [
   {
     key: '1',
     name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
+    chinese: 98,
+    math: 60,
+    english: 70,
   },
   {
     key: '2',
     name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
+    chinese: 98,
+    math: 66,
+    english: 89,
   },
   {
     key: '3',
     name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
+    chinese: 98,
+    math: 90,
+    english: 70,
   },
   {
     key: '4',
     name: 'Jim Red',
-    age: 32,
-    address: 'London No. 2 Lake Park',
+    chinese: 88,
+    math: 99,
+    english: 89,
   },
 ];
 
